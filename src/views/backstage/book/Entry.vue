f2930e759c1269dcc39e6ee20842183071a006d8
TANG8464 <1784998369@qq.com>
2020-07-17 18:37:58 +0800

    a

--- src/views/backstage/book/Entry.vue
+++ src/views/backstage/book/Entry.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="">
-    <div style="margin:23px ">
+  <div class>
+    <div style="margin:23px 0">
       <el-breadcrumb separator-class="el-icon-arrow-right" style="width:150px">
         <el-breadcrumb-item style="font-size:18px;">录入书籍</el-breadcrumb-item>
       </el-breadcrumb>
@@ -27,26 +27,44 @@
               <el-input v-model.number="form.count" autocomplete="off"></el-input>
             </el-form-item>
             <el-form-item label="分类号" prop="bookCategoryID">
-              <el-input v-model="form.bookCategoryID" autocomplete="off"></el-input>
+              <!-- <el-input v-model="form.bookCategoryID" autocomplete="off"></el-input> -->
+              <el-cascader :props="props" :show-all-levels="false" v-model="form.bookCategoryID"></el-cascader>
             </el-form-item>
-
             <el-form-item label="ISBN" prop="isbn">
               <el-input v-model="form.isbn"></el-input>
             </el-form-item>
           </div>
-          <div class="cover">
-            <el-upload
-              class="avatar-uploader"
-              action="https://jsonplaceholder.typicode.com/posts/"
-              :show-file-list="false"
-              :on-success="handleAvatarSuccess"
-              :before-upload="beforeAvatarUpload"
-              :headers="token"
+          <div>
+            <el-form
+              :model="form"
+              :label-width="formLabelWidth"
+              label-position="top"
+              :rules="rules"
             >
-              <img v-if="imageUrl" :src="imageUrl" class="avatar" />
-              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-            </el-upload>
-            <p>书籍封面</p>
+              <el-form-item label="书籍封面" ref="uploadElement" prop="img">
+                <el-upload
+                  ref="upload"
+                  action="http://120.26.177.203/news/picturesUpload"
+                  accept="image/png, image/gif, image/jpg, image/jpeg"
+                  list-type="picture-card"
+                  :limit="limitNum"
+                  :auto-upload="true"
+                  :on-exceed="handleExceed"
+                  :before-upload="handleBeforeUpload"
+                  :on-preview="handlePictureCardPreview"
+                  :on-remove="handleRemove"
+                  :on-change="imgChange"
+                  :class="{hide:hideUpload}"
+                  :headers="token"
+                  :on-success="handleAvatarSuccess"
+                >
+                  <i class="el-icon-plus"></i>
+                </el-upload>
+                <el-dialog :visible.sync="dialogVisible">
+                  <img width="100%" :src="dialogImageUrl" alt />
+                </el-dialog>
+              </el-form-item>
+            </el-form>
           </div>
         </div>
 
@@ -61,7 +79,9 @@
 <script>
 export default {
   data() {
+    let _this = this;
     var checkNum = (rule, value, callback) => {
+      console.log(value);
       if (value <= 0) {
         callback(new Error("必须大于0"));
       } else {
@@ -69,9 +89,9 @@ export default {
       }
     };
     return {
-      token:{},
+      token: {},
       isImg: false,
-      imageUrl: "",
+      isSuccess: false,
       form: {
         bookName: "",
         author: "",
@@ -79,7 +99,38 @@ export default {
         price: 0,
         count: 0,
         bookCategoryID: "",
-        remark: ""
+        remark: "",
+        imageUrl: ""
+      },
+      props: {
+        lazy: true,
+        lazyLoad(node, resolve) {
+          let resdata = {};
+          const id = node.root ? null : node.value;
+          if (id != null) {
+            resdata = { categoryId: id };
+          }
+          console.log(id);
+          console.log(resdata);
+          _this.$axios
+            .get("book/categorise", {
+              params: resdata
+            })
+            .then(res => {
+              if (res.data.data.length == 0) {
+                node.data.leaf = true;
+                console.log(node.data.leaf);
+              }
+              const nodes = Array.from(res.data.data).map(item => ({
+                value: item.id,
+                label: item.bookCategoryName,
+                leaf: false
+              }));
+              // console.log(nodes)
+              resolve(nodes);
+            })
+            .catch(error => {});
+        }
       },
       rules: {
         bookName: [{ required: true, message: "书名不能为空" }],
@@ -94,11 +145,17 @@ export default {
           { required: true, message: "数量不能为空" },
           { type: "number", message: "数量必须为数字值" },
           { validator: checkNum, trigger: "blur" }
-        ]
-      }
+        ],
+        img: [{ required: true, message: "请选择封面" }]
+      },
+      hideUpload: false,
+      dialogImageUrl: "",
+      dialogVisible: false, //图片预览弹窗
+      formLabelWidth: "80px",
+      limitNum: 1
     };
   },
-  created(){
+  created() {
     this.getToken();
   },
   methods: {
@@ -106,33 +163,67 @@ export default {
       //获取登录时存储在localStorage中的header-Token，作为上传凭证
       this.token["HEADER-TOKEN"] = localStorage.getItem("HEADER_TOKEN");
     },
-    handleAvatarSuccess(res, file) {
-      this.imageUrl = URL.createObjectURL(file.raw);
-      this.isImg = true;
+    // 上传文件之前的钩子
+    handleBeforeUpload(file) {
+      if (
+        !(
+          file.type === "image/png" ||
+          file.type === "image/gif" ||
+          file.type === "image/jpg" ||
+          file.type === "image/jpeg"
+        )
+      ) {
+        this.$notify.warning({
+          title: "警告",
+          message:
+            "请上传格式为image/png, image/gif, image/jpg, image/jpeg的图片"
+        });
+      }
+      let size = file.size / 1024 / 1024 / 2;
+      if (size > 2) {
+        this.$notify.warning({
+          title: "警告",
+          message: "图片大小必须小于2M"
+        });
+      }
     },
-    beforeAvatarUpload(file) {
-      const isJPG = file.type === "image/jpeg";
-      const isLt2M = file.size / 1024 / 1024 < 2;
-
-      if (!isJPG) {
-        this.$message.error("上传头像图片只能是 JPG 格式!");
+    // 文件超出个数限制时的钩子
+    handleExceed(files, fileList) {},
+    // 文件列表移除文件时的钩子
+    handleRemove(file, fileList) {
+      this.hideUpload = fileList.length >= this.limitNum;
+    },
+    // 点击文件列表中已上传的文件时的钩子
+    handlePictureCardPreview(file) {
+      this.dialogImageUrl = file.url;
+      console.log(file.url);
+      this.dialogVisible = true;
+    },
+    handleAvatarSuccess(res, file) {
+      this.form.imageUrl = res.link;
+      this.isSuccess = true;
+    },
+    imgChange(files, fileList) {
+      this.hideUpload = fileList.length >= this.limitNum;
+      if (fileList) {
+        this.$refs.uploadElement.clearValidate();
       }
-      if (!isLt2M) {
-        this.$message.error("上传头像图片大小不能超过 2MB!");
-      }
-      return isJPG && isLt2M;
     },
     onSubmit(formName) {
-      // if (!this.isImg) {
-      //   this.$message.error("请选择书籍封面");
-      //   return;
-      // }
+      this.form.bookCategoryID=this.form.bookCategoryID[this.form.bookCategoryID.length-1]
+      console.log(this.form.bookCategoryID)
+      if (!this.isSuccess) {
+        this.$message.error("请选择书籍封面");
+        return;
+      }
+  
       this.$refs[formName].validate(async valid => {
         if (valid) {
           if (this.form.remark == "") {
             this.form.remark = null;
           }
-          console.log(this.form.remark);
+
+          console.log(this.form.imageUrl);
           const { data: res } = await this.$http.post("book/", this.form);
           if (res.code != 200) {
             this.$message.error("提交失败");
@@ -152,54 +243,17 @@ export default {
   }
 };
 </script>
-<style scoped>
-/* .form {
-  margin-top: 50px;
-  margin-left: 30px;
-  
-} */
+<style >
 .el-input {
   width: 350px;
-  
-}
-.main {
-  padding: 40px 80px;
 }
 .form1 {
   display: flex;
+  flex-direction: row;
   justify-content: space-between;
 }
-.cover {
-  margin-left: 100px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  color: #787878;
-  margin-top: 50px;
-}
-.avatar-uploader {
-  border: 1px dashed #d9d9d9;
-  border-radius: 6px;
-  cursor: pointer;
-  position: relative;
-  overflow: hidden;
-  height: auto;
-  width: auto;
-}
-.avatar-uploader:hover {
-  border-color: #409eff;
-}
-.avatar-uploader-icon {
-  font-size: 28px;
-  color: #8c939d;
-  width: 178px;
-  height: 178px;
-  line-height: 178px;
-  text-align: center;
-}
-.avatar {
-  width: 178px;
-  height: 178px;
-  display: block;
+
+.hide .el-upload--picture-card {
+  display: none;
 }
 </style>
